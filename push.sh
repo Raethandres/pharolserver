5e974b232460000aa2c0cffb5c40f7145c7e84d3
Raethandres <raethandres4@gmail.com>
2019-03-15 07:39:32 -0500

    Delete push.sh

--- push.sh
+++
@@ -1,3 +0,0 @@
-docker build -t pharol-web:latest .
-docker tag pharol-web:latest 876200339127.dkr.ecr.us-east-1.amazonaws.com/pharolweb:latest
-docker push ecr.us-east-1.amazonaws.com/pharolweb:latest
